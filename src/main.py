286cb0498f0ea692355fae475b74255e5899a605
htkg <howtokillgarry@gmail.com>
2025-02-22 11:44:41 +0300

    ✨ Refactor main.py to load credentials from environment variables and pass them to DatabaseClient

--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
 import asyncio
 from pyrogram import Client, idle
-import os
 from src.utils.logging import setup_structlog
 from src.database.client import DatabaseClient
+from src.utils.credentials import Credentials
 
 # Setup logging once at the module level
 logger = setup_structlog()
 
 async def main():
+    # Load credentials
+    credentials = Credentials.from_env()
+    
     # Initialize and connect to database
-    conn = DatabaseClient()
+    conn = DatabaseClient(credentials)
     try:
         await conn.connect()
         
         app = Client(
-            "nexus",
-            api_id=int(os.getenv("APP_ID")), 
-            api_hash=os.getenv("APP_HASH"),  
-            bot_token=os.getenv("BOT_TOKEN"),
+            credentials.bot.name,
+            api_id=credentials.bot.app_id,
+            api_hash=credentials.bot.app_hash,
+            bot_token=credentials.bot.bot_token,
             plugins=dict(root="src/plugins"),
             mongodb=dict(connection=conn.client, remove_peers=False))
         
