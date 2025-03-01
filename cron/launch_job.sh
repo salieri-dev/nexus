217509a48be32bbbebe0840be3b2c2be3f536876
htkg <akudryashov.work@gmail.com>
2025-03-01 12:04:21 +0000

    🔨 Scripts

---
+++ cron/launch_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Simple script to start the sentiment analysis container
+
+# Set the working directory to the project root
+cd "$(dirname "$0")/.." || exit 1
+
+# Start the cron container
+docker-compose up -d --no-deps cron
